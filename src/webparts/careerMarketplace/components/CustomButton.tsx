87958f811af637b8d0b521a001e4bcba9e6c62c7
Adi Makkar <adimakkar.ca@gmail.com>
2024-10-31 15:58:41 -0400

    changes to customebutton for pipeline purposes

--- src/webparts/careerMarketplace/components/CustomButton.tsx
+++ src/webparts/careerMarketplace/components/CustomButton.tsx
@@ -1,32 +1,22 @@
 import * as React from 'react';
-import { DefaultButton, PrimaryButton, ThemeProvider} from '@fluentui/react';
+import { DefaultButton, PrimaryButton, ThemeProvider } from '@fluentui/react';
 
 export interface ICustomButtonProps {
   id: string;
   name: string;
   buttonType: 'primary' | 'secondary';
-  onClick: (value:any) => void ;
+  onClick: (value: HTMLButtonElement) => void;
 }
 
-
 export default class CustomButton extends React.Component<ICustomButtonProps> {
-
   
-
-
-  private handleOnClick = (ev?: any): void => {
-    console.log("clicked",ev.target)
-    const buttonName: any= ev.target
-    this.props.onClick(buttonName)
+  private handleOnClick = (ev: React.MouseEvent<HTMLButtonElement>): void => {
+    console.log("clicked", ev.currentTarget);
+    this.props.onClick(ev.currentTarget); 
   }
-   
-
-
+  
   public render(): React.ReactElement<ICustomButtonProps> {
-
-
     return (
-      <>
       <ThemeProvider>
         <div>
           {
@@ -35,19 +25,15 @@ export default class CustomButton extends React.Component<ICustomButtonProps> {
               <div id={this.props.id}>
                 <PrimaryButton text={this.props.name} onClick={this.handleOnClick} />
               </div>
-            ):
+            ) :
             (
-              <div id={this.props.id} >
-                <DefaultButton text={this.props.name} style={{color: '#03787c'}} onClick={this.handleOnClick} />
+              <div id={this.props.id}>
+                <DefaultButton text={this.props.name} style={{ color: '#03787c' }} onClick={this.handleOnClick} />
               </div>
             )
           }
-
         </div> 
-        </ThemeProvider>
-      </>
-      
-
-    )
+      </ThemeProvider>
+    );
   }
 }
