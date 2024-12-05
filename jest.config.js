d0d5380ed8475927154b2df3ffad1cfc527c709f
Adi Makkar <adimakkar.ca@gmail.com>
2024-12-05 12:33:37 -0500

    wip - updated jest requirements

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
+const localizedResourcesPath = config.localizedResources.CareerMarketplaceWebpartWebPartStrings;
+
+module.exports = {
+  setupFiles: ["<rootDir>/jest.setup.ts"],
+  verbose: true,
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.[t|j]sx?$": "ts-jest",
+  },
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "json"
+  ],
+  testMatch: [
+    "**/src/**/*.test.+(ts|tsx|js)"
+  ],
+
+  moduleNameMapper: {
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+    "^CareerMarketplaceWebpartWebPartStrings$": `<rootDir>/${localizedResourcesPath.replace('{locale}', 'en-us')}`
+  }, 
+  reporters: [
+    'default', 
+    ['jest-junit', {
+      outputDirectory: './test-results',
+      outputName: 'junit.xml'
+    }]
+  ]
+};
