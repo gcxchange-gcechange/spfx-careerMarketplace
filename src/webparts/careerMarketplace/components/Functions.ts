d55d27bc41b142b488620cbddaa11d1917932a95
gabmor38 <gabriela.morenor@gmail.com>
2024-12-05 14:09:17 -0500

    fixed lint issue

--- src/webparts/careerMarketplace/components/Functions.ts
+++ src/webparts/careerMarketplace/components/Functions.ts
@@ -1,11 +1,11 @@
 
 
-export const toTitleCase = ( value: any): string[] => {
+export const toTitleCase = ( value: {key: string, value: string | number}[]): string[] => {
 
   const properCaseValues: any[] = [];
 
 
-  const convertString = value.map((item: any) => item.key.replace(/([A-Z])/g, ' $1').replace(/^ /, '').toLowerCase() );
+  const convertString = value.map((item: {key: string, value: string | number}) => item.key.replace(/([A-Z])/g, ' $1').replace(/^ /, '').toLowerCase() );
  
   properCaseValues.push(convertString);
 
