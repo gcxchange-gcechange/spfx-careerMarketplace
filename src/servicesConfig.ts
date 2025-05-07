de6c4114508cbdebbbcb28cd4a9d4d6742f7fdea
gabmor38 <gabriela.morenor@gmail.com>
2025-05-07 15:53:36 -0400

    centrelized api urls, parameters and jobOppList url

--- src/servicesConfig.ts
+++ src/servicesConfig.ts
@@ -1,9 +1,24 @@
+/* eslint-disable @rushstack/no-new-null */
 export interface ICreateOpportunity {
   clientId: string;
-  apiUrl:string;
+  createJobApiUrl:string;
 
 }
 
 export interface IEditOpportunity {
   apiUrlEdit: string;
+}
+
+export const createOpportunityConfig = {
+  clientId: "c121f403",
+  createJobApiUrl: "api/CreateJobOpportunity",
+  editJobApiUrl:"api/UpdateJobOpportunity",
+  jobTypeTermId: "45f37f08-3ff4-4d84-bf21-4a77ddffcf3e",
+  programAreaTermId:"bd807536-d8e7-456b-aab0-fae3eecedd8a",
+};
+
+const baseJobOpportunityUrl = "";
+
+export const getJobOpportunityUrl = (jobOpportunityId: string | null ):string => {
+  return `${baseJobOpportunityUrl}?JobOpportunityId=${jobOpportunityId}`
 }
